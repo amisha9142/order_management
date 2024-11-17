eb4b3e9ebfa767816fe048166c9951367d3e6275
Amisha Raj <amisharaj107@gmail.com>
2024-11-17 22:27:09 +0530

    testing

--- Frontend/src/component/Sidebar.jsx
+++ Frontend/src/component/Sidebar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="w-64 bg-gray-800 text-white p-4">
-      <h2 className="text-3xl font-bold text-center mb-8">Admin Panel</h2>
+      <h2 className="text-3xl font-bold text-start mb-8">Panel</h2>
       <nav>
         <ul>
           <li>
